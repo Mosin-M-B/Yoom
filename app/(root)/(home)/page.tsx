701e75566d02ab7e6c9b9bc65308285c1aa09790
Mosin Balsing <mosinbalsing@gmail.com>
2024-09-26 17:18:56 +0530

    remove dependancy array

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -50,7 +50,7 @@ const Home = () => {
   useEffect(() => {
     const closest = findClosestMeeting();
     setClosestMeeting(closest);
-  }, [upcomingCalls]);
+  });
 
   // Get only the time for the closest meeting
   const upcomingMeetingTime = useMeetingTime(closestMeeting);
